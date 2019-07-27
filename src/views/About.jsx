65a7b4d0213034fbf06f828f22b693c055d07d0d
Michael Cronin <mcronin_03@yahoo.com>
2019-07-26 20:27:43 -0400

    Update About.jsx

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Divider } from '../elements/Dividers'
+import { Divider, DividerMiddle } from '../elements/Dividers'
 import Content from '../elements/Content'
 import Inner from '../elements/Inner'
 import { UpDown, UpDownWide } from '../styles/animations'
@@ -9,29 +9,37 @@ import SVG from '../components/SVG'
 
 const About = ({ children, offset }) => (
   <>
-    <Divider bg="#23262b" clipPath="polygon(0 16%, 100% 4%, 100% 82%, 0 94%)" speed={0.2} offset={offset} />
-    <Divider speed={0.1} offset={offset}>
-      <UpDown>
-        <SVG icon="box" hiddenMobile width={6} fill={colors.blue} left="50%" top="75%" />
-        <SVG icon="upDown" hiddenMobile width={8} fill={colors['grey-darkest']} left="70%" top="20%" />
-        <SVG icon="triangle" width={8} stroke={colors['grey-darkest']} left="25%" top="5%" />
-        <SVG icon="upDown" hiddenMobile width={24} fill={colors.orange} left="80%" top="80%" />
-      </UpDown>
-      <UpDownWide>
-        <SVG icon="arrowUp" hiddenMobile width={16} fill={colors.purple} left="5%" top="80%" />
-        <SVG icon="triangle" width={12} stroke={colors.white} left="95%" top="50%" />
-        <SVG icon="circle" width={6} fill={colors.white} left="85%" top="15%" />
-        <SVG icon="upDown" hiddenMobile width={8} fill={colors['grey-darkest']} left="45%" top="10%" />
-      </UpDownWide>
-      <SVG icon="circle" width={6} fill={colors.white} left="4%" top="20%" />
-      <SVG icon="circle" width={12} fill={colors['grey-darkest']} left="70%" top="60%" />
-      <SVG icon="box" width={6} fill={colors.orange} left="10%" top="10%" />
-      <SVG icon="box" width={12} fill={colors['grey-darkest']} left="20%" top="30%" />
-      <SVG icon="hexa" width={8} stroke={colors['grey-darkest']} left="80%" top="70%" />
-    </Divider>
-    <Content speed={0.4} offset={offset}>
+    {/*<DividerMiddle
+      bg="linear-gradient(to left, white 0%, white 100%)"
+      speed={0.1}
+      offset={`.8`}
+      factor={1.35}
+    />*/}
+    <Content speed={.7} offset={.85} factor={2}>
       <Inner>{children}</Inner>
     </Content>
+    <Divider speed={0.1} offset={offset} factor={2}>
+      {/*<UpDown>
+        <SVG icon="box" width={6} fill={colors.black} left="85%" top="75%" />
+        <SVG icon="upDown" width={8} fill={colors.yellow} left="70%" top="20%" />
+        <SVG icon="triangle" width={8} stroke={colors.white} left="25%" top="5%" />
+        <SVG icon="circle" hiddenMobile width={24} fill={colors.midnightBlue} left="10%" top="60%" />
+      </UpDown>
+      <UpDownWide>
+        <SVG icon="arrowUp" hiddenMobile width={16} fill={colors.green} left="20%" top="90%" />
+        <SVG icon="triangle" width={12} stroke={colors.teal} left="90%" top="30%" />
+        <SVG icon="circle" width={16} fill={colors.yellow} left="70%" top="90%" />
+        <SVG icon="triangle" hiddenMobile width={16} stroke={colors.teal} left="18%" top="75%" />
+        <SVG icon="circle" width={6} fill={colors.white} left="75%" top="10%" />
+        <SVG icon="upDown" hiddenMobile width={8} fill={colors.green} left="45%" top="10%" />
+      </UpDownWide>
+      <SVG icon="circle" width={6} fill={colors.black} left="4%" top="20%" />
+      <SVG icon="circle" width={12} fill={colors.pink} left="85%" top="60%" />
+      <SVG icon="box" width={6} fill={colors.chocolate} left="10%" top="10%" />
+      <SVG icon="box" width={12} fill={colors.chocolate} left="29%" top="26%" />
+      <SVG icon="hexa" width={16} stroke={colors.red} left="75%" top="30%" />
+      <SVG icon="hexa" width={10} stroke={colors.chocolate} left="80%" top="70%" />*/}
+    </Divider>
   </>
 )
 
