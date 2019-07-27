ca891caa0d8810b12f3cd55eea4594625fbcb4f3
Michael Cronin <mcronin_03@yahoo.com>
2019-07-26 20:29:58 -0400

    Update Contact.jsx

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -13,40 +13,13 @@ const WaveWrapper = styled.div`
   transform: matrix(1, 0, 0, -1, 0, 0);
 `
 
-const InnerWave = styled.div`
-  ${tw`relative h-full`};
-  svg {
-    width: 100%;
-    height: 40vh;
-  }
-  path {
-    ${waveAnimation('20s')};
-  }
-`
-
 const Contact = ({ children, offset }) => (
   <>
-    <Divider fill="#23262b" speed={0.2} offset={offset}>
-      <WaveWrapper>
-        <InnerWave>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 338.05" preserveAspectRatio="none">
-            <path>
-              <animate
-                attributeName="d"
-                values="M 0 100 Q 250 50 400 200 Q 550 350 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 200 150 400 200 Q 600 250 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 150 350 400 200 Q 650 50 800 300 L 800 0 L 0 0 L 0 100 Z"
-                repeatCount="indefinite"
-                dur="30s"
-              />
-            </path>
-          </svg>
-        </InnerWave>
-      </WaveWrapper>
-    </Divider>
-    <Content speed={0.4} offset={offset}>
+    <Content speed={0.1} offset={offset}>
       {children}
     </Content>
     <Divider speed={0.1} offset={offset}>
-      <UpDown>
+      {/*<UpDown>
         <SVG icon="upDown" hiddenMobile width={8} fill={colors['grey-darkest']} left="70%" top="20%" />
         <SVG icon="triangle" width={8} stroke={colors['grey-darkest']} left="25%" top="5%" />
       </UpDown>
@@ -58,7 +31,7 @@ const Contact = ({ children, offset }) => (
       <SVG icon="circle" width={6} fill={colors.white} left="4%" top="20%" />
       <SVG icon="circle" width={12} fill={colors['grey-darkest']} left="70%" top="60%" />
       <SVG icon="box" width={12} fill={colors['grey-darkest']} left="20%" top="30%" />
-      <SVG icon="hexa" width={8} stroke={colors['grey-darkest']} left="80%" top="70%" />
+      <SVG icon="hexa" width={8} stroke={colors['grey-darkest']} left="80%" top="70%" />*/}
     </Divider>
   </>
 )
