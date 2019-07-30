6fdeabfbfed9e2330ff01b453b60cbb9d7d0cb11
Michael Cronin <mcronin_03@yahoo.com>
2019-07-30 15:26:32 -0400

    simplified google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,14 +15,6 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-123638133-2",
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: false,
-        // Setting this parameter is also optional
-        respectDNT: false
-        // Avoids sending pageview hits from custom paths
-        // exclude: ["/preview/**", "/do-not-track/me/too/"],
       },
     },
     {
