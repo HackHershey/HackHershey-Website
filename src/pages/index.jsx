7d405d51ede7e4a1e948265d796034dfec632d29
Michael Cronin <mcronin_03@yahoo.com>
2019-08-01 00:18:55 -0400

    add form

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -316,6 +316,28 @@ const Index = () => (
         </Inner>
       </Contact>
 
+      <form name="contact" method="post" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
+
+                    <input type="hidden" name="bot-field" />
+
+                    <div className="field half first">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" id="name" required/>
+                    </div>
+                    <div className="field half">
+                        <label htmlFor="email">Email</label>
+                        <input type="text" name="email" id="email" required/>
+                    </div>
+                    <div className="field">
+                        <label htmlFor="message">Message</label>
+                        <textarea name="message" id="message" rows="6" required></textarea>
+                    </div>
+                    <ul className="actions">
+                        <li><input type="submit" value="Send Message" className="special" /></li>
+                        <li><input type="reset" value="Clear" /></li>
+                    </ul>
+                </form>
+
       <FooterText>
         Made with ❤️️ by The HackHershey Team.{' '} <br></br>
         Check out our <a href="https://github.com/mcronin03/HackHershey-Website">Github Repository</a>.
