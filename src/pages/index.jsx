bf30f7536553a454ddb37d7953eeb581de508263
Michael Cronin <mcronin_03@yahoo.com>
2019-08-08 15:55:30 -0400

    new form

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -200,7 +200,24 @@ const FooterText = styled.footer`
   background: midnightBlue;
   transition: transform 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275);
   `
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch((error) => alert(error))
+  }
 
 const Index = () => (
   <>
@@ -209,14 +226,46 @@ const Index = () => (
       <Hero offset={0}>
         <BigTitle style={{ textAlign:'center'}}>HackHershey</BigTitle>
         <Subtitle style={{ textAlign:'center'}}>Hershey High School's very own hackathon!<br /><br /> Date TBD</Subtitle>
-        <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true" action="/thanks">
-          <input type="hidden" name="bot-field" />
+        <form
+        name="contact"
+        method="post"
+        action="/thanks/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
+      >
+        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
           <label>
-            Email
-            <input type="email" name="email" id="email" />
+            Don’t fill this out: <input name="bot-field" onChange={handleChange} />
           </label>
+        </p>
+        <p>
+          <label>
+            Your name:
+            <br />
+            <input type="text" name="name" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your email:
+            <br />
+            <input type="email" name="email" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message:
+            <br />
+            <textarea name="message" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
           <button type="submit">Send</button>
-        </form>
+        </p>
+      </form>
       </Hero>
 
       <AboutSpacer></AboutSpacer>
