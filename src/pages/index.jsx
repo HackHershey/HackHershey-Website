6aab7d987e73de013c0b801b2b184ba5a7921007
Michael Cronin <mcronin_03@yahoo.com>
2019-08-17 20:05:05 -0400

    more mobile spacing

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -132,7 +132,7 @@ display: block;
 }
 `
 const SponsorSpacer = styled.div`
-${tw`flex flex-wrap justify-between sm:py-24 md:py-4 lg:py-8`};
+${tw`flex flex-wrap justify-between sm:py-16 md:py-4 lg:py-8`};
 display: block;
 @media (max-width: 1200px) {
   grid-gap: 3rem;
@@ -143,7 +143,7 @@ display: block;
 }
 `
 const ContactSpacer = styled.div`
-${tw`flex flex-wrap justify-between pt-16 md:p-8 lg:py-10 xl:pt-0`};
+${tw`flex flex-wrap justify-between pt-12 md:p-8 lg:py-10 xl:pt-0`};
 display: block;
 @media (max-width: 1200px) {
   grid-gap: 3rem;
