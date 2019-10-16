d5ac4bc508df8570ee4750deb59bb3039f3a436b
Michael Cronin <mcronin_03@yahoo.com>
2019-10-15 23:28:20 -0400

    Update FAQCard.jsx

--- src/components/FAQCard.jsx
+++ src/components/FAQCard.jsx
@@ -14,7 +14,7 @@ const Wrapper = styled.a`
   }
 `
 const Text = styled.div`
-  ${tw`opacity-75 font-sans text-xxs lg:text-s xl:text-base text-midnightBlue`};
+  ${tw`opacity-75 font-sans text-xxs lg:text-sm xl:text-base text-midnightBlue`};
 `
 const Title = styled.div`
   ${tw`text-midnightBlue uppercase text-sm lg:text-lg xl:text-xl tracking-wide font-sans -mt-8`};
