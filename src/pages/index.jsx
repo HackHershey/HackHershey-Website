8adb5c5b720d56f54980f309b4c92705a252d93d
Michael Cronin <mcronin_03@yahoo.com>
2019-11-16 14:27:35 -0500

    add form

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -219,6 +219,7 @@ const Index = () => (
       <Hero offset={0}>
         <BigTitle style={{ textAlign:'center'}}>HackHershey</BigTitle>
         <Subtitle style={{ textAlign:'center'}}>Hershey High School's very own hackathon!<br /><br /> December 14, 2019</Subtitle>
+        <a class="typeform-share button" href="https://michaelcronin248127.typeform.com/to/FeBqx2" data-mode="drawer_left" style="display:inline-block;text-decoration:none;background-color:#267DDD;color:white;cursor:pointer;font-family:Helvetica,Arial,sans-serif;font-size:20px;line-height:50px;text-align:center;margin:0;height:50px;padding:0px 33px;border-radius:25px;max-width:100%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;font-weight:bold;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;" target="_blank">Launch me </a> <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script>
       </Hero>
 
       <AboutSpacer></AboutSpacer>
