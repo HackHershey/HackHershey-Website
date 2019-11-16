b3e90f2ecab265d71a915a5e273b75f49291cedd
Michael Cronin <mcronin_03@yahoo.com>
2019-11-16 16:52:37 -0500

    new form link

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -219,7 +219,7 @@ const Index = () => (
       <Hero offset={0}>
         <BigTitle style={{ textAlign:'center'}}>HackHershey</BigTitle>
         <Subtitle style={{ textAlign:'center'}}>Hershey High School's very own hackathon!<br /><br /> December 14, 2019</Subtitle>
-        <html> <head> <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"> <title>Event Registration</title> <style type="text/css"> html{ margin: 0; height: 100%; overflow: hidden; } iframe{ position: absolute; left:0; right:0; bottom:0; top:0; border:0; } </style> </head> <body> <iframe id="typeform-full" width="100%" height="100%" frameborder="0" src="https://michaelcronin248127.typeform.com/to/FeBqx2"></iframe> <script type="text/javascript" src="https://embed.typeform.com/embed.js"></script> </body> </html>
+        <Subtitle style={{ textAlign:'center'}}><a href="https://hackhershey.typeform.com/to/FeBqx2" target="_blank" rel="noopener">Sign Up Now!</a></Subtitle>
       </Hero>
 
       <AboutSpacer></AboutSpacer>
